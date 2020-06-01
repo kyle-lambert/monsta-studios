21b25440779a46066853cb5d76a76e39f7ab6ff3
kylelambert001 <kylelambert001@gmail.com>
2020-06-01 17:06:02 +0930

    added noscroll functionality during overlay animation and completed overlay animation method

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,29 @@
-import "../scss/global.scss";
+import "../scss/home.scss";
+import gsap from "gsap";
+
+const overlayAnimation = (node1, node2) => {
+  const tl = gsap.timeline();
+  tl.from(node1, {
+    duration: 1,
+    opacity: 0,
+    scale: 2,
+    ease: "expo.out",
+    delay: 1,
+  }).to(node2, {
+    duration: 1.5,
+    ease: "expo.in",
+    top: "100%",
+    delay: 1,
+    display: "none",
+    onComplete: () => {
+      document.body.classList.remove("noscroll");
+    },
+  });
+};
+
+(function () {
+  const overlay = document.querySelector("[data-overlay]");
+  const overlayLogo = document.querySelector("[data-overlay-logo]");
+
+  overlayAnimation(overlayLogo, overlay);
+})();
